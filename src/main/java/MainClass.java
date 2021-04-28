3bb4d123b77c92419dc175b0d59041478a70848e
sandra.kirtovska <sandra.kirtovska@office.ctco.lv>
2021-04-28 11:15:30 +0300

    Initial

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,79 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter input file name with full path to it : ");
+        String path = input.nextLine();
+        System.out.println(path);
+
+        ArrayList<String> rows = new ArrayList<String>();
+        ArrayList<Integer> digits = new ArrayList<Integer>();
+
+        try {
+            File inputFile = new File(path);
+            Scanner inputScanner = new Scanner(inputFile);
+            int i = 0;
+
+            while ( inputScanner.hasNextLine()) {
+
+                if (inputScanner.hasNextInt()){
+                    int b =  inputScanner.nextInt();
+                    digits.add(b);
+
+                }else{
+                    String data = inputScanner.nextLine();
+                    rows.add(data);
+
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+
+//      1. Все строки склеенные в одну
+//        System.out.println(rows.size());
+
+        String concatAll = "";
+
+        for (int k = 0;k < rows.size();k++)
+        {
+            concatAll += rows.get(k);
+        }
+        System.out.println();
+        System.out.println("1. All lines are concatenated to one: " + concatAll);
+
+//      2. Сумма всех чисел
+        int sum = 0;
+
+        for (int j=0;j < digits.size();j++)
+        {
+            sum += digits.get(j);
+        }
+        System.out.println();
+        System.out.println("2. Total is  " + sum);
+
+//      3. Количество чётных чисел
+        int n = 0;
+        int c = 0;
+        while(n < digits.size()){
+            int k = digits.get(n);
+            int rest = k%2;
+            n++;
+            if (rest==0)
+            {
+                c++;
+            }
+        }
+        System.out.println();
+        System.out.println("3. The number of even numbers is " + c);
+
+    }
+}
